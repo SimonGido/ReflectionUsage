4d2e2b307f1c5188e161c10476a622ea1f621deb
Šimon Gido <simongido1@gmail.com>
2023-04-19 16:27:36 +0200

    usage example

--- WalnutApp/src/WalnutApp.cpp
+++ WalnutApp/src/WalnutApp.cpp
@@ -4,10 +4,24 @@
 
 #include "Reflection.h"
 
+#include "../entt/include/entt/entt.hpp"
+
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
 using namespace XYZ;
+
+static void DrawUI(const char* name, std::string& value)
+{
+	char nameBuffer[_MAX_PATH];
+	memcpy(nameBuffer, value.c_str(), value.size());
+	nameBuffer[value.size()] = 0;
+
+	if (ImGui::InputText(name, nameBuffer, _MAX_PATH))
+	{
+		value = nameBuffer;
+	}
+}
 
 static void DrawUI(const char* name, glm::vec3& value)
 {
@@ -31,12 +45,17 @@ static void DrawComponent(const char* name, T& component)
 {
 	ImGui::Text(name);
 	Utils::For<Reflection<T>::sc_NumVariables>([&](auto i) {
-		std::string variableName = std::string(Reflection<T>::sc_VariableNames[i.value]);
+		auto variableName = Reflection<T>::GetName<i.value>();
 		auto& variable = Reflection<T>::Get<i.value>(component);
-		DrawUI(variableName.c_str(), variable);
+		DrawUI(variableName.data(), variable);
 	});
 }
 
+struct NameComponent
+{
+	std::string Name;
+};
+REFLECTABLE(NameComponent, Name)
 
 struct TransformComponent
 {
@@ -60,40 +79,57 @@ REFLECTABLE(BoxColliderComponent, Size, Offset)
 
 
 
-REGISTER_REFLECTABLES(COMPONENTS, TransformComponent, BoxColliderComponent)
+REGISTER_REFLECTABLES(COMPONENTS, NameComponent, TransformComponent, BoxColliderComponent)
 
 class ExampleLayer : public Walnut::Layer
 {
 public:
-	TransformComponent transform;
-	BoxColliderComponent boxCollider;
+	entt::registry m_Registry;
+
+	std::vector<entt::entity> m_Entities;
+
+	ExampleLayer()
+	{
+		entt::entity entity = m_Registry.create();
+		m_Registry.emplace<NameComponent>(entity);
+		m_Registry.emplace<TransformComponent>(entity);
+		m_Registry.emplace<BoxColliderComponent>(entity);
+		m_Entities.push_back(entity);
+	}
 
 	virtual void OnUIRender() override
 	{
 		if (ImGui::Begin("Components"))
-		{		
-			Utils::For<COMPONENTS::sc_NumClasses>([&](auto i) {
+		{
+			for (auto entity : m_Entities)
+			{
+				Utils::For<COMPONENTS::sc_NumClasses>([&](auto i) {
 
-				std::string className = std::string(COMPONENTS::sc_ClassNames[i.value]);
-				auto reflClass = COMPONENTS::Get<i.value>();
-				auto& component = GetComponent(reflClass);
-				DrawComponent(className.c_str(), component);
-			});		
+					auto reflClass = COMPONENTS::Get<i.value>();
+					if (HasComponent(entity, reflClass))
+					{
+						auto className = COMPONENTS::GetName<i.value>();
+						auto& component = GetComponent(entity, reflClass);
+
+						const char* name = className.data();
+						DrawComponent(name, component);
+					}
+				});
+			}
 		}
 		ImGui::End();
 	}
 
 	template <typename T>
-	T& GetComponent(Reflection<T> refl)
+	T& GetComponent(entt::entity entity, Reflection<T> refl)
 	{
-		if constexpr (std::is_same_v<T, TransformComponent>)
-		{
-			return transform;
-		}
-		else
-		{
-			return boxCollider;
-		}
+		return m_Registry.get<T>(entity);
+	}
+
+	template <typename T>
+	bool HasComponent(entt::entity entity, Reflection<T> refl)
+	{
+		return m_Registry.any_of<T>(entity);
 	}
 
 };
